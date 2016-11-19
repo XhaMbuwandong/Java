1ccb5d27335acd017af39c0b9053b7adf7166d61
Xha Mbuwandong <gleidsonrgr@gmail.com>
2016-11-19 15:21:04 +0000

    Enviando meus primeiro arquivo .java no cloud 9.

---
+++ testes/Hello.java
@@ -0,0 +1,12 @@
+/*Primeiro programa em Java no cloud 9.*/
+
+
+public class Hello {
+
+    public static void main (String [] args) {
+        
+        System.out.println ("Hello World!!");
+    
+    } /*fim da função main.*/
+    
+} /*fim da classe Hello.*/
